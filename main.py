48f6fe5ad35ea67f877f5356dd8c68999df1be2b
Ramiro Yoffe <ryoffe@udesa.edu.ar>
2026-04-06 11:56:12 -0300

    fix: update main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.carga_datos import cargar_datos
-from src.metricas import calcular_fc_desde_datos, calcular_maximo_senal, calcular_minimo_senal, calcular_amplitud_senal
-from src.procesamiento_datos import *
+from src.metricas import calcular_fc_desde_datos, calcular_maximo_senal, calcular_minimo_senal, calcular_amplitud_senal, calcular_promedio
+from src.procesamiento_datos import filtrar_por_participante
 from src.validacion_datos import *
 
 ruta = './datos/datos_proyecto.csv'
@@ -9,12 +9,18 @@ i_d = '001'
 datos = cargar_datos(ruta)
 
 promedio = calcular_promedio(datos)
-participante = filtrar_por_participante(datos, i_d)
 fc = calcular_fc_desde_datos(datos)
 senal_max, senal_min = calcular_maximo_senal(datos), calcular_minimo_senal(datos)
 amplitud = calcular_amplitud_senal(senal_max, senal_min)
 
 print(f'El promedio de la senal es: {promedio}')
-print(f'Se encontro a un participante con el id {id}')
 print(f'La frequencia cardiaca promedio es de {fc}Hz')
 print(f'El maximo de la señal es de {senal_max} y el minimo es de {senal_min}')
+
+
+try:
+    participante = filtrar_por_participante(datos, i_d)
+except Exception as e:
+    print(e)
+else:
+    print(f'Se encontro a un participante con el id {participante["id"]}')

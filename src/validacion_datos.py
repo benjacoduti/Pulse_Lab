ff270753a252bac1563ac33920260ccc81f67693
Ramiro Yoffe <ryoffe@udesa.edu.ar>
2026-04-10 16:39:14 -0300

    Fix: Update validar_dato()

--- src/validacion_datos.py
+++ src/validacion_datos.py
@@ -21,8 +21,41 @@ def to_float_or_str(dato):
     except ValueError:
         return dato
 
-def validar_registro(registro):
-    
-    # Esta función aparece entre las principales para entregar, hay que pensar con qué la usariamos
-    
-    pass
+def validar_dato(dato, i):
+    """
+    Recibe un dato perteneciente a una linea del archivo y su posicion y valida los tipos de los datos
+    :param dato: Un dato especifico de la linea
+    :return: Linea del archivo validada y casteada al tipo de dato correspondiente
+    """
+    try:
+        if i < 3:
+            if i == 0:
+                dato = int(dato)
+            elif i == 1 or i==2:
+                dato = float(dato)
+            if dato > 0:
+                return dato
+            else:
+                raise ValueError("El dato es negativo")
+        elif i == 3:
+            if dato == "tarea" and dato == "baseline":
+                return dato
+            else:
+                raise ValueError(f"El dato en la posicion {i+1} no es una de las opciones validas")
+        elif i == 4:
+            if dato == "cooperacion" and dato == "competencia":
+                return dato
+            else:
+                raise ValueError(f"El dato en la posicion {i+1} no es una de las opciones validas")
+        elif i == 5:
+            if dato == "True":
+                return True
+            elif dato == "False":
+                return False
+            else:
+                raise ValueError
+    except ValueError as e:
+        if e is None:
+            raise  ValueError(f"El dato en la posicion {i+1} no es del tipo especificado")
+        else:
+            raise e
